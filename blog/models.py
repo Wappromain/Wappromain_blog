fa9463aed8c7479c3079d167c0a9781c453d7844
Patrick Wande <ayomikunwande@gmail.com>
2022-06-15 13:34:19 -0500

    Created blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title
+
+
